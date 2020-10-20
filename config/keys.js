e969c4c0228ea56ace40c97433c7f6b8dba0b424
Udeep <31485423+udeep1995@users.noreply.github.com>
2020-10-20 09:32:36 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://node_assgn_user:FAH3sbhEz2IV58KD@generic.jajr2.mongodb.net/nodejs_assign?retryWrites=true&w=majority",
+    mongoURI: ""  // please use any mongo db key to run this app,
     secret: "super_secret"
 }
